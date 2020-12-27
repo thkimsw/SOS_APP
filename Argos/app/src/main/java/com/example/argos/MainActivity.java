fc371dfac7589f2eb8cacd12439b46012656f847
thkimsw <thkim.sw98@gmail.com>
2020-12-27 21:33:28 +0900

    semi

--- Argos/app/src/main/java/com/example/argos/MainActivity.java
+++ Argos/app/src/main/java/com/example/argos/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.argos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int GPS_ENABLE_REQUEST_CODE = 2001;
     private static final int PERMISSIONS_REQUEST_CODE = 100;
     String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-    Button distict,emergency,setting;
+    Button emergency,setting,submit,kakao;
     Switch c_state;
 
     @Override
@@ -23,31 +25,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         gpsTracker = new GpsTracker(MainActivity.this);
         //thread 가속도 센서
-
-        distict = findViewById(R.id.distict);
         emergency = findViewById(R.id.emergency);
         setting = findViewById(R.id.setting);
         c_state = findViewById(R.id.c_state);
-
-        
-        distict.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+        submit = findViewById(R.id.submit);
+        kakao = findViewById(R.id.kakao);
 
         emergency.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //비상벨
             }
         });
 
         setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                //비상연락망 세팅
+                Intent set = new Intent(getApplication(),peoples.class);
+                startActivity(set);
             }
         });
 
@@ -55,6 +51,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 // on,off
+                //지속적 위치정보 보내기
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //긴급신고
+
+                String message = "보낼 내용";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.putExtra("sms_body", message );
+                intent.setType("vnd.android-dir/mms-sms");
+                startActivity(intent);
+
+            }
+        });
+        kakao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = "보낼 내용";
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, message );
+                intent.setPackage("com.kakao.talk");
+                startActivity(intent);
             }
         });
     }
